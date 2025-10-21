a22bf5a8d3dce66019b7b9bf47f5ca2e566b4091
Conor <conorweir58@gmail.com>
2025-10-21 14:01:44 +0100

    Created and implemented Transition class

---
+++ fsa/src/Transition.java
@@ -0,0 +1,31 @@
+public class Transition
+{
+    // Class Attributes
+    private final char symbol;
+    private final State fromState;
+    private final State toState;
+
+    // Constructor
+    public Transition(State fromState, char symbol, State toState)
+    {
+        this.symbol = symbol;
+        this.fromState = fromState;
+        this.toState = toState;
+    }
+
+    // Getters
+    public char getSymbol()
+    {
+        return this.symbol;
+    }
+
+    public State getFromState()
+    {
+        return this.fromState;
+    }
+
+    public State getToState()
+    {
+        return this.toState;
+    }
+}
